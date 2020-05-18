1b43cdfb52c694096a8022fd0416ec4dfa9f0a52
Your Name <kris.kay.design@gmail.com>
2020-05-18 17:08:38 -0400

    modal works

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+ //Request 
+ document.querySelector('.request').addEventListener('click', event => {
+    document.querySelector('#requestModal').classList.add('open')
+  })
+
+//Request X
+document.querySelector('#modalX').addEventListener('click', event => {
+    document.querySelector('#requestModal').classList.remove('open')
+  })
+
+ 
