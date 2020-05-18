affa60bfe20f360f2650dca60af79ab80933b7ff
Your Name <kris.kay.design@gmail.com>
2020-05-18 17:39:49 -0400

    Click to copy email

--- js/main.js
+++ js/main.js
@@ -9,4 +9,24 @@ document.querySelector('#modalX').addEventListener('click', event => {
     document.querySelector('#requestModal').classList.remove('open')
   })
 
- 
+//Copy Email Btn 
+var copyEmailBtn = document.querySelector('.js-emailcopybtn');  
+copyEmailBtn.addEventListener('click', function(event) {  
+// Select the email link anchor text  
+var emailLink = document.querySelector('.js-emaillink');  
+var range = document.createRange();  
+range.selectNode(emailLink);  
+window.getSelection().addRange(range);  
+
+try {  
+//copy command  
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+} catch(err) {  
+    console.log('Oops, unable to copy');  
+}  
+
+// Remove the selections  
+window.getSelection().removeAllRanges();  
+});
