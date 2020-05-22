283bc38e6427fb43318da736475a893804f15a49
Your Name <kris.kay.design@gmail.com>
2020-05-21 20:33:07 -0400

    dark mode first draft

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
 
- //Request 
- document.querySelector('.request').addEventListener('click', event => {
-    document.querySelector('#requestModal').classList.add('open')
+//Light/Dark Mode btn
+document.querySelector('#navLogo').addEventListener('click', event => {
+    document.querySelector('html').classList.toggle('dark')
+
+    document.querySelector('body').classList.toggle('dark')
+
+    const $darkEle = document.querySelectorAll('.dark')
+    const $lightEle = document.querySelectorAll('.light')
+    
+
+    $darkEle.forEach($dark => {
+        $dark.classList.toggle('hidden')
+      })
+
+    $lightEle.forEach($light => {
+    $light.classList.toggle('hidden')
+    })
+
+    // $logo = document.querySelector('#navLogo')
+    // $wordLogo = document.querySelector('#tagWordLogo')
+    // $mask = document.querySelector('#mask')
+    // $bra = document.querySelector('#bra')
+    // $pan = document.querySelector('#panty')
+  
+
+
+    // document.querySelector('#navLogoImg').setAttribute("src", "img/logo-peach-noWord-blueBg-LLAYLLA.svg")
   })
 
-//Request X
+//Request modal open
+document.querySelector('.request').addEventListener('click', event => {
+document.querySelector('#requestModal').classList.add('open')
+})
+
+//Request modal X
 document.querySelector('#modalX').addEventListener('click', event => {
     document.querySelector('#requestModal').classList.remove('open')
   })
 
-//Copy Email Btn 
+//Copy to clipboard Btn 
 var copyEmailBtn = document.querySelector('.js-emailcopybtn');  
 copyEmailBtn.addEventListener('click', function(event) {  
-// Select the email link anchor text  
+    // Select the email link anchor text  
 var emailLink = document.querySelector('.js-emaillink');  
 var range = document.createRange();  
 range.selectNode(emailLink);  
 window.getSelection().addRange(range);  
 
 try {  
-//copy command  
+    //copy command  
     var successful = document.execCommand('copy');  
     var msg = successful ? 'successful' : 'unsuccessful';  
     console.log('Copy email command was ' + msg);  
@@ -27,6 +56,6 @@ try {
     console.log('Oops, unable to copy');  
 }  
 
-// Remove the selections  
-window.getSelection().removeAllRanges();  
+        // Remove the selections  
+        window.getSelection().removeAllRanges();  
 });
